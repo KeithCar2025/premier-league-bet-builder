0c0e82a814eacf9fb7c2ba68f5126b09b8b656f5
KeithCar2025 <keithcarpenter01@gmail.com>
2026-03-20 23:26:05 +0000

    Create BetBuilder class in modules/bet_builder.py

---
+++ modules/bet_builder.py
@@ -0,0 +1,16 @@
+class BetBuilder:
+    def __init__(self):
+        # Initialize your BetBuilder class here
+        pass
+
+    def add_bet(self, bet):
+        # Logic to add a bet
+        pass
+
+    def calculate_potential_winnings(self):
+        # Logic to calculate potential winnings
+        pass
+
+    def clear_bets(self):
+        # Logic to clear all bets
+        pass
